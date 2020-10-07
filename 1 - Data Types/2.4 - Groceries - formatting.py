e5a2c06b5d659a13c442c74c2b1a6470cbf9cb60
LuckyGitHub777 <71360116+LuckyGitHub777@users.noreply.github.com>
2020-10-07 16:58:57 -0700

    Add files via upload

---
+++ 1 - Data Types/2.4 - Groceries - formatting.py
@@ -0,0 +1,6 @@
+#   Formatting
+#   Edit your code above, so your snacks print with a space in between each one.
+#   Add commas before each space.
+
+favorite_snack = "FavoriteSnack"
+print((favorite_snack + ', ')* 123 )
---
+++ 1 - Data Types/2.5 - Groceries - bonus grocery list.py
@@ -0,0 +1,10 @@
+#   BONUS (Grocery List Extended)
+#   Can you figure out how to get the same output as Q1 above with only 1 print statement? If so, can you do it all on one line?
+#   From Grocery List: Print 5 things you need from the grocery store.
+#       print("Thing1")
+#       print("Thing2")
+#       print("Thing3")
+#       print("Thing4")
+#       print("Thing5")
+
+print("Thing1\nThing2\n Thing3\n Thing4\nThing5\n")
