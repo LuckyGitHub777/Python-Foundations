37df99a923dea5de53e907d0dd716c0d06d96f4c
LuckyGitHub777 <71360116+LuckyGitHub777@users.noreply.github.com>
2020-10-07 17:00:13 -0700

    Delete Groceries - grocery list.py

--- 1 - Data Types/Groceries - grocery list.py
+++
@@ -1,8 +0,0 @@
-#   Grocery List
-#   Print 5 things you need from the grocery store.
-
-print("Thing1:Milk")
-print("Thing2:Eggs")
-print("Thing3:Bread")
-print("Thing4:Bananas")
-print("Thing5:Oatmeal")
