98600084f1675e5953ace16ceb9184b5929ea44f
LuckyGitHub777 <71360116+LuckyGitHub777@users.noreply.github.com>
2020-10-07 17:00:00 -0700

    Delete Snackin - favorite snack.py

--- 1 - Data Types/Snackin - favorite snack.py
+++
@@ -1,6 +0,0 @@
-#   Favorite Snack
-#   Declare a variable called favorite_snack and set its value to be your favorite snack.
-#   Print out the string "My favorite snack is <YOUR_SNACK>" using the variable favorite_snack that you created.
-
-favorite_snack = 'FavoriteSnackHere'
-print ('My favorite snack is '+ favorite_snack)
