55e4cbf1de29c99145ddaa8d96d2782d8f768a8d
LuckyGitHub777 <71360116+LuckyGitHub777@users.noreply.github.com>
2020-10-07 17:00:17 -0700

    Delete Groceries - buy lots.py

--- 1 - Data Types/Groceries - buy lots.py
+++
@@ -1,8 +0,0 @@
-#   Buy Lots
-#   Pick your favorite snack. Use the * (multiplication) operator to print 123 copies for it.
-#   Use the * to calculate the final price for those 123 snacks and print the total cost.
-
-favorite_snack = "FavoriteSnack"
-cost = 1.11
-print(favorite_snack * 123)
-print(cost * 123)
